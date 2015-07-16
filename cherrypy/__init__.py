397543edbe95ebe6953299bbe4353a1e6ef67c7c
Kevin Way <kway@monetate.com>
2015-07-16 13:18:49 -0400

    update version

--- cherrypy/__init__.py
+++ cherrypy/__init__.py
@@ -57,7 +57,7 @@ These API's are described in the CherryPy specification:
 http://www.cherrypy.org/wiki/CherryPySpec
 """
 
-__version__ = "3.1.2.monetate3"
+__version__ = "3.1.2.monetate4"
 
 from urlparse import urljoin as _urljoin
 
